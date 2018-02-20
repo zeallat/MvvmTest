2ad36532b19bdae7e7b39a9d7167446e4ceb2d37
HoJunLee <zeallat94@gmail.com>
2018-02-20 13:19:09 +0900

    test

--- app/src/main/java/com/appg/mvvmtest/MainActivity.java
+++ app/src/main/java/com/appg/mvvmtest/MainActivity.java
@@ -4,13 +4,23 @@ import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.appg.mvvmtest.data.User;
+import com.appg.mvvmtest.databinding.MainActivityBinding;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_activity);
 //        MainActivityDa
-        DataBindingUtil.setContentView(this, R.layout.activity_main);
+        MainActivityBinding binding =
+                DataBindingUtil.setContentView(this, R.layout.main_activity);
+        User user = new User();
+        user.setAge(1);
+        user.setName("이름이름");
+        user.setPhoneNumber("010010101010");
+
+        binding.setUser(user);
     }
 }
